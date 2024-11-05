bd17a4bcd37b1cee719cc77a055eafa8cf5e1b09
arlonlab <arlonlabalan@outlook.com>
2024-11-05 08:58:52 +0100

    Setup

---
+++ absence/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx,html}", // Ensure this covers all your source files
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
