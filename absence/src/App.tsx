3a925f1a69dda6ea8ec8229620ad5f607151db1b
arlonlab <arlonlabalan@outlook.com>
2024-11-19 10:00:07 +0100

    Sprint 1

--- absence/src/App.tsx
+++ absence/src/App.tsx
@@ -1,9 +1,11 @@
 import NavBar from "./components/NavBar";
+import Upload from "./components/Upload";
 function App() {
 
   return (
     <>
     <NavBar></NavBar>
+    <Upload></Upload>
       </>
 
   );
